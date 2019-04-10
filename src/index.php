c9371577c222e8238e7d3e8da076d04fcc904ee0
arthur.djikpo <arthur.djikpo@gmail.com>
2019-04-10 14:45:07 +0200

    env + index.php

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo 'Hello el mondo ';
+
+// $U0 = 2;
+// $R = -2;
+
+// echo '<hr>';
+// echo $U0."<p>\n</p>";
+// for($i = 0; $i < 10 ; $i=$i+1){
+
+//     $U= $U0*$R;
+//     echo $U."<p>\n</p>";
+//     $U0=$U;
+// }
+// echo '<hr>';
+
+// $pattern  = '#^[a-z\.]+@[a-z]{2,}\.[a-z]{2,}$#i';
+// $email = 'nom.prenom@email.fr';
+// $resul = preg_match($pattern, $email);
+// print_r($resul);
